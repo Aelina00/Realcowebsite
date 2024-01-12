e01fdd2c35d491cd6cdcacda72e3b2ec45f45ada
Aelina00 <sugar.w.0iiol@gmail.com>
2024-01-12 09:03:54 +0300

    added section intro

--- core.js
+++ core.js
@@ -98,3 +98,17 @@ function update_position(e) {
 
 addEventListener('mouseover', update_position);
 addEventListener('mouseout', update_position)
+
+
+// sign in
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
