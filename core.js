f4669d1ace9ba4f646bac21f0cd34caeae2ac756
Aelina00 <sugar.w.0iiol@gmail.com>
2024-01-12 09:43:16 +0300

    added js code to add active class in selected thmbnails in intro section

--- core.js
+++ core.js
@@ -118,6 +118,16 @@ function imgSlider(anything) {
 document.querySelector('.property').src = anything;
 }
 function changeBgColor(color) {
-    const bg = document.querySelector('.bg');
-    bg.style.background = color;
+  const bg = document.querySelector('.bg');
+  bg.style.background = color;
+}
+let el = document.querySelectorAll('.thumb li');
+for(let i = 0; i < el.length; i++) {
+  el[i].onclick = function() {
+    var c = 0;
+    while (c < el.length) {
+      el[c++].className = 'check';
+    }
+    el[i].className = 'check active'
   }
+}
