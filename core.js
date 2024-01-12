5dd3d4419eaad87d143e1bd89a3675ac063f8f94
Aelina00 <sugar.w.0iiol@gmail.com>
2024-01-12 09:41:44 +0300

    added js code to change image on click

--- core.js
+++ core.js
@@ -112,3 +112,8 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+// intro
+function imgSlider(anything) {
+document.querySelector('.property').src = anything;
+}
