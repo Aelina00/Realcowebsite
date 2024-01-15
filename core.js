0e4b7546edabd7398eb5381b9f28f828e59f98ae
Aelina00 <sugar.w.0iiol@gmail.com>
2024-01-15 11:04:15 +0300

    added preloader,music

--- core.js
+++ core.js
@@ -1,3 +1,63 @@
+
+// header
+// Open and Close Navbar Menu
+const navbarMenu = document.getElementById("menu");
+const burgerMenu = document.getElementById("burger");
+const bgOverlay = document.querySelector(".overlay");
+
+if (burgerMenu && bgOverlay) {
+   burgerMenu.addEventListener("click", () => {
+      navbarMenu.classList.add("is-active");
+      bgOverlay.classList.toggle("is-active");
+   });
+
+   bgOverlay.addEventListener("click", () => {
+      navbarMenu.classList.remove("is-active");
+      bgOverlay.classList.toggle("is-active");
+   });
+}
+
+// Close Navbar Menu on Links Click
+document.querySelectorAll(".menu-link").forEach((link) => {
+   link.addEventListener("click", () => {
+      navbarMenu.classList.remove("is-active");
+      bgOverlay.classList.remove("is-active");
+   });
+});
+
+// Open and Close Search Bar Toggle
+const searchBlock = document.querySelector(".search-block");
+const searchToggle = document.querySelector(".search-toggle");
+const searchCancel = document.querySelector(".search-cancel");
+
+if (searchToggle && searchCancel) {
+   searchToggle.addEventListener("click", () => {
+      searchBlock.classList.add("is-active");
+   });
+
+   searchCancel.addEventListener("click", () => {
+      searchBlock.classList.remove("is-active");
+   });
+}
+
+// preloader
+window.addEventListener('load', () => {
+
+const preloader = document.querySelector('.preloader');
+const content = document.querySelector('.content');
+  
+preloader.classList.add('hide-preloader');
+  
+setTimeout(() => {
+	content.style.display = 'block';
+}, 5000);
+  
+setTimeout(() => {
+	preloader.style.display = 'none'; 
+}, 15000);
+  
+});
+
 // button
 // const button = document.querySelector( 'button' );
 // const distanceBetween = ( p1x, p1y, p2x, p2y ) => {
@@ -45,46 +105,7 @@ setTimeout(function () {
     content.style.display = 'none';
 }, 10000);
 
-// header
-// Open and Close Navbar Menu
-const navbarMenu = document.getElementById("menu");
-const burgerMenu = document.getElementById("burger");
-const bgOverlay = document.querySelector(".overlay");
 
-if (burgerMenu && bgOverlay) {
-   burgerMenu.addEventListener("click", () => {
-      navbarMenu.classList.add("is-active");
-      bgOverlay.classList.toggle("is-active");
-   });
-
-   bgOverlay.addEventListener("click", () => {
-      navbarMenu.classList.remove("is-active");
-      bgOverlay.classList.toggle("is-active");
-   });
-}
-
-// Close Navbar Menu on Links Click
-document.querySelectorAll(".menu-link").forEach((link) => {
-   link.addEventListener("click", () => {
-      navbarMenu.classList.remove("is-active");
-      bgOverlay.classList.remove("is-active");
-   });
-});
-
-// Open and Close Search Bar Toggle
-const searchBlock = document.querySelector(".search-block");
-const searchToggle = document.querySelector(".search-toggle");
-const searchCancel = document.querySelector(".search-cancel");
-
-if (searchToggle && searchCancel) {
-   searchToggle.addEventListener("click", () => {
-      searchBlock.classList.add("is-active");
-   });
-
-   searchCancel.addEventListener("click", () => {
-      searchBlock.classList.remove("is-active");
-   });
-}
 
 // btn
 function update_position(e) {
@@ -115,90 +136,6 @@ signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
 
-// intro
-function imgSlider(anything) {
-document.querySelector('.property').src = anything;
-}
-function changeBgColor(color) {
-  const bg = document.querySelector('.bg');
-  bg.style.background = color;
-}
-let el = document.querySelectorAll('.thumb li');
-for(let i = 0; i < el.length; i++) {
-  el[i].onclick = function() {
-    var c = 0;
-    while (c < el.length) {
-      el[c++].className = 'check';
-    }
-    el[i].className = 'check active'
-  }
-}
-
-(function($) { "use strict";
-
-    document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
-        t.style.left = n.clientX + "px", 
-		t.style.top = n.clientY + "px", 
-		e.style.left = n.clientX + "px", 
-		e.style.top = n.clientY + "px", 
-		i.style.left = n.clientX + "px", 
-		i.style.top = n.clientY + "px"
-    });
-    var t = document.getElementById("cursor"),
-        e = document.getElementById("cursor2"),
-        i = document.getElementById("cursor3");
-    function n(t) {
-        e.classList.add("hover"), i.classList.add("hover")
-    }
-    function s(t) {
-        e.classList.remove("hover"), i.classList.remove("hover")
-    }
-    s();
-    for (var r = document.querySelectorAll(".hover-target"), a = r.length - 1; a >= 0; a--) {
-        o(r[a])
-    }
-    function o(t) {
-        t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
-    }
-	
-
-	var app = function () {
-		var body = undefined;
-		var menu = undefined;
-		var menuItems = undefined;
-		var init = function init() {
-			body = document.querySelector('body');
-			menu = document.querySelector('.menu-icon');
-			menuItems = document.querySelectorAll('.nav__list-item');
-			applyListeners();
-		};
-		var applyListeners = function applyListeners() {
-			menu.addEventListener('click', function () {
-				return toggleClass(body, 'nav-active');
-			});
-		};
-		var toggleClass = function toggleClass(element, stringClass) {
-			if (element.classList.contains(stringClass)) element.classList.remove(stringClass);else element.classList.add(stringClass);
-		};
-		init();
-	}();
-
-	
-	$("#switch").on('click', function () {
-		if ($("body").hasClass("light")) {
-			$("body").removeClass("light");
-			$("#switch").removeClass("switched");
-		}
-		else {
-			$("body").addClass("light");
-			$("#switch").addClass("switched");
-		}
-	});
-	
-})(jQuery); 
-
-
- 
 
 
 // comment
@@ -266,3 +203,24 @@ for(let i = 0; i < el.length; i++) {
 	
 })(jQuery); 
 
+$(document).ready(function() {
+    $('#user-form').validate({
+   rules: {
+     email: {
+       required: true,
+       email: true
+     },
+   },
+   
+   highlight: function(element, errorClass, validClass) { 
+     $(element).nextAll('.form-control-feedback').show().removeClass('glyphicon-ok').addClass('glyphicon-remove');
+     $(element).addClass(errorClass).removeClass(validClass);
+     $(element).closest('.form-group').removeClass('has-success').addClass('has-error');
+   },
+   success: function(element) {
+     $(element).nextAll('.form-control-feedback').show().removeClass('glyphicon-remove').addClass('glyphicon-ok');
+  element.closest('.form-group').removeClass('has-error').addClass('has-success');
+     $(element).remove();
+   }
+ });
+}); 
